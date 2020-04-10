9dd62f2ea2fb319a5d56e2b426c4e76cb7438d67
chaudharyaditya <cloud.aditya.d.chaudhary@gmail.com>
2020-04-09 22:21:51 -0500

    Initial commit

---
+++ src/app/productalert/productalert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-productalert',
+  templateUrl: './productalert.component.html',
+  styleUrls: ['./productalert.component.css']
+})
+export class ProductalertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
